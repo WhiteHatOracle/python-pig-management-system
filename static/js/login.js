4b1c6d7376a5fa4671e5cf0bfebbb6f590fbab05
Taonga Phiri <danieltphiri9@gmail.com>
2025-03-03 12:27:32 +0200

    updated landing page

--- static/js/login.js
+++ static/js/login.js
@@ -3,8 +3,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const slides = Array.from(track.children);
     const nextButton = document.querySelector(".next");
     const prevButton = document.querySelector(".prev");
-    const dotsNav = document.querySelector(".carousel__nav");
-    const dots = Array.from(dotsNav.children);
 
     let slideWidth = slides[0].getBoundingClientRect().width; // Get slide width
 
@@ -20,48 +18,22 @@ document.addEventListener("DOMContentLoaded", () => {
         targetSlide.classList.add("current-slide");
     };
 
-    const updateDots = (currentDot, targetDot) => {
-        currentDot.classList.remove("current-slide");
-        targetDot.classList.add("current-slide");
-    };
-
     // Move to next slide
     nextButton.addEventListener("click", () => {
         const currentSlide = track.querySelector(".current-slide");
-        const currentDot = dotsNav.querySelector(".current-slide");
 
         const nextSlide = currentSlide.nextElementSibling || slides[0]; // Loop to first slide
-        const nextDot = currentDot.nextElementSibling || dots[0];
 
         moveToSlide(track, currentSlide, nextSlide);
-        updateDots(currentDot, nextDot);
     });
 
     // Move to previous slide
     prevButton.addEventListener("click", () => {
         const currentSlide = track.querySelector(".current-slide");
-        const currentDot = dotsNav.querySelector(".current-slide");
 
         const prevSlide = currentSlide.previousElementSibling || slides[slides.length - 1]; // Loop to last slide
-        const prevDot = currentDot.previousElementSibling || dots[dots.length - 1];
 
         moveToSlide(track, currentSlide, prevSlide);
-        updateDots(currentDot, prevDot);
-    });
-
-    // Click on navigation dots
-    dotsNav.addEventListener("click", (e) => {
-        const targetDot = e.target;
-        if (!dots.includes(targetDot)) return; // Ignore clicks outside buttons
-
-        const currentSlide = track.querySelector(".current-slide");
-        const currentDot = dotsNav.querySelector(".current-slide");
-
-        const targetIndex = dots.findIndex((dot) => dot === targetDot);
-        const targetSlide = slides[targetIndex];
-
-        moveToSlide(track, currentSlide, targetSlide);
-        updateDots(currentDot, targetDot);
     });
 
     // Recalculate slide positions if window resizes
