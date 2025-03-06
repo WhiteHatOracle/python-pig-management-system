ba74633c7c53569b2b57b5979ceb8fd9f0111a2f
Taonga Phiri <danieltphiri9@gmail.com>
2025-03-06 11:58:29 +0200

    push

--- static/js/login.js
+++ static/js/login.js
@@ -1,44 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const track = document.querySelector(".carousel__track");
-    const slides = Array.from(track.children);
-    const nextButton = document.querySelector(".next");
-    const prevButton = document.querySelector(".prev");
+function toggleMenu() {
+    const menu = document.getElementById("mobile-menu");
+    const menuIcon = document.getElementById("menu-icon");
+    const closeIcon = document.getElementById("close-icon");
+    
+    if (menu.classList.contains("hidden")) {
+        menu.classList.remove("hidden");
+        menuIcon.style.display = "none";
+        closeIcon.style.display = "block";
+        document.addEventListener("click", closeMenuOutside);
+    } else {
+        menu.classList.add("hidden");
+        menuIcon.style.display = "block";
+        closeIcon.style.display = "none";
+        document.removeEventListener("click", closeMenuOutside);
+    }
+}
 
-    let slideWidth = slides[0].getBoundingClientRect().width; // Get slide width
-
-    // Arrange slides next to each other
-    const setSlidePosition = (slide, index) => {
-        slide.style.left = `${slideWidth * index}px`;
-    };
-    slides.forEach(setSlidePosition);
-
-    const moveToSlide = (track, currentSlide, targetSlide) => {
-        track.style.transform = `translateX(-${targetSlide.style.left})`;
-        currentSlide.classList.remove("current-slide");
-        targetSlide.classList.add("current-slide");
-    };
-
-    // Move to next slide
-    nextButton.addEventListener("click", () => {
-        const currentSlide = track.querySelector(".current-slide");
-
-        const nextSlide = currentSlide.nextElementSibling || slides[0]; // Loop to first slide
-
-        moveToSlide(track, currentSlide, nextSlide);
-    });
-
-    // Move to previous slide
-    prevButton.addEventListener("click", () => {
-        const currentSlide = track.querySelector(".current-slide");
-
-        const prevSlide = currentSlide.previousElementSibling || slides[slides.length - 1]; // Loop to last slide
-
-        moveToSlide(track, currentSlide, prevSlide);
-    });
-
-    // Recalculate slide positions if window resizes
-    window.addEventListener("resize", () => {
-        slideWidth = slides[0].getBoundingClientRect().width;
-        slides.forEach(setSlidePosition);
-    });
-});
+function closeMenuOutside(event) {
+    const menu = document.getElementById("mobile-menu");
+    const menuIcon = document.querySelector(".mobile-menu-icon");
+    
+    if (!menu.contains(event.target) && !menuIcon.contains(event.target)) {
+        menu.classList.add("hidden");
+        document.getElementById("menu-icon").style.display = "block";
+        document.getElementById("close-icon").style.display = "none";
+        document.removeEventListener("click", closeMenuOutside);
+    }
+}
