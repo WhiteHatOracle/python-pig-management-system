a0b834d2f485f0f0e207eca8f40b3ef83079308e
Taonga Phiri <danieltphiri9@gmail.com>
2025-11-11 12:38:59 +0200

    commit

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from authlib.integrations.flask_client import OAuth
 from sqlalchemy.engine import Engine
 from sqlalchemy.exc import IntegrityError
-from sqlalchemy.exc import IntegrityError
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
